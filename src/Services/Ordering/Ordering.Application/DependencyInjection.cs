7ebcc084f68d40d3c06d6a38f470acab3512e3ce
a.mirzaei <ashkanmv@gmail.com>
2023-02-11 16:17:02 +0330

    services injected

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection service)
+        {
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+            service.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return service;
+        }
+    }
+}
